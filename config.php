b88d61841c1e6434dd0a2483b2d16ea3c5faf42c
Vasile Bejan <124290984+vasile007@users.noreply.github.com>
2025-07-14 13:13:32 +0100

    Update config.php

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ define('DB_PASS', '');
 define('DB_NAME', 'sweet_treats_db');
 
 define('ADMIN_EMAIL', 'admin@sweettreats.com');
-define('ADMIN_PASSWORD_HASH', '$2y$10$WvQts.yB8KSnBhM4UXaEvOxEj3U5kFV45AfDKqRMNl0T0GYOXUlOS'); // hash pt "admin123"
+define('ADMIN_PASSWORD_HASH', '$2y$10$WvQts.yB8KSnBhM4UXaEvOxEj3U5kFV45AfDKqRMNl0T0GYOXUlOS'); 
 
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($conn->connect_error) {
