c9861c4e6253ea1f89b62ec85afac4f0cc3460fc
Vasile Bejan <124290984+vasile007@users.noreply.github.com>
2025-07-14 13:15:11 +0100

    Update test_password.php

--- test_password.php
+++ test_password.php
@@ -3,7 +3,7 @@ $input_password = 'admin123';
 $hash = '$2y$10$kD5KQi8UqTRhAx1uBgtceOHL.H7bdEJTeJCSfV20vV5TR6Xcc0P5a';
 
 if (password_verify($input_password, $hash)) {
-    echo "Parola este corectă!";
+    echo "Your password!";
 } else {
-    echo "Parola este greșită!";
+    echo "Wrong password!";
 }
